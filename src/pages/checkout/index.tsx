5932a4c902fac9fbc2e8a53e3601026496f2bd26
David Sanchez <d4vsanchez@gmail.com>
2021-10-31 17:58:18 -0500

    Initial Checkout structure

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from 'next'
 
+import styles from '@/styles/Checkout.module.css'
+
 const Checkout: NextPage = () => {
-  return <h1>Checkout</h1>
+  return (
+    <div className={styles.container}>
+      <div>
+        <h3>Lista de Pedidos</h3>
+        {/* Checkout item */}
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+        {/* Checkout item */}
+      </div>
+
+      <div className={styles.sidebar}>
+        <h3>
+          Precio Total: <span>$10</span>
+        </h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
+  )
 }
 
 export default Checkout
