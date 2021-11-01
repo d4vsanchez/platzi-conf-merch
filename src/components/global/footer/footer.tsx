a33ef1c890cd9bdef15b813c69ca5d05fac0ed8d
David Sanchez <d4vsanchez@gmail.com>
2021-10-31 20:05:21 -0500

    Add Tailwind to footer

--- src/components/global/footer/footer.tsx
+++ src/components/global/footer/footer.tsx
@@ -1,9 +1,9 @@
+import 'twin.macro'
 import type { FC } from 'react'
-import styles from './Footer.module.css'
 
 export const Footer: FC = () => {
   return (
-    <footer className={styles.footer}>
+    <footer tw="flex justify-between items-center py-4 mt-6">
       <p>PlatziConf Merch</p>
       <p>Todos los izquierdos reservados</p>
     </footer>
